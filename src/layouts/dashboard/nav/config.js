bc7fb0610183fc5b8707991b68248da52d03a53f
Haovvh <haovvh@gmail.com>
2023-06-08 21:33:12 +0700

    Save Report

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -9,18 +9,18 @@ const navConfig = [
   {
     title: 'dashboard',
     path: '/app',
-    icon: icon('ic_analytics'),
+    icon: icon('ic_5'),
   },
   
   {
     title: 'Report',
     path: '/report',
-    icon: icon('ic_user'),
+    icon: icon('ic_analytics'),
   },
   {
     title: 'Campaign',
     path: '/campaign',
-    icon: icon('ic_cart'),
+    icon: icon('ic_4'),
   },
   {
     title: 'Store',
@@ -30,32 +30,32 @@ const navConfig = [
   {
     title: 'ProductCategory',
     path: '/productCategory',
-    icon: icon('ic_cart'),
+    icon: icon('ic_2'),
   }, 
   {
     title: 'ProductItem',
     path: '/productitem',
-    icon: icon('ic_cart'),
+    icon: icon('ic_3'),
   }, 
   {
     title: 'Voucher',
     path: '/voucher',
-    icon: icon('ic_cart'),
+    icon: icon('ic_8'),
   },
   {
     title: 'Game',
     path: '/game',
-    icon: icon('ic_cart'),
+    icon: icon('ic_disabled'),
   },
   {
     title: 'Profile',
     path: '/profile',
-    icon: icon('ic_cart'),
+    icon: icon('ic_user'),
   } ,
   {
     title: 'Logout',
     path: '/logout',
-    icon: icon('ic_cart'),
+    icon: icon('ic_lock'),
   } 
 ];
 
